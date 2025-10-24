69f0c3ad1d2dff3cbe65cc8e51253b7190089219
N0\A <salary-hazels8c@icloud.com>
2025-10-24 20:15:24 +0200

    better header

--- core/headers.py
+++ core/headers.py
@@ -13,7 +13,7 @@ def get_useragent() -> str:
         "Mozilla/5.0 (Windows NT 10.0; WOW64; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
         "Mozilla/5.0 (Windows NT 10.0; WOW64; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240",
         "IE11; Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko",
-        "CLARA /1.0 (whatever)",
+        "CLARA /1.0 (Your friendly desktop assistant)",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82"
     ]
     import random
